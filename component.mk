2bc26d82d8752f766bbc7197dfabecaeabe9ac82
Arun <arun@siara.cc>
2018-12-31 16:12:46 +0530

    move assert.h osapi.h to private_include

--- component.mk
+++ component.mk
@@ -1,7 +1,6 @@
 #
 # Component Makefile
 #
-# (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
-
+COMPONENT_PRIV_INCLUDEDIRS=private_include
 CFLAGS = -std=gnu99 -g3 -fno-stack-protector -ffunction-sections -fdata-sections -fstrict-volatile-bitfields -mlongcalls -nostdlib -Wpointer-arith -Wno-error=unused-function -Wno-error=unused-but-set-variable -Wno-error=unused-variable -Wno-error=deprecated-declarations -Wno-unused-parameter -Wno-sign-compare -Wno-old-style-declaration -MMD -c -DF_CPU=240000000L -DESP32 -DCORE_DEBUG_LEVEL=0 -DNDEBUG
 
